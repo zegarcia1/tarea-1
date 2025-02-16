52b95017091a0923646375abe119879449e2524e
zegarcia1 <sebts84@gmail.com>
2025-02-16 12:46:22 -0500

    Add files via upload

--- CalcularMayor4.java
+++ CalcularMayor4.java
@@ -13,16 +13,16 @@ public class CalcularMayor4 {
         System.out.print("Ingresa un número entero: ");
         int numero4 = scanner.nextInt();
 
-        if (numero1>numero2 && numero1> numero3){
+        if (numero1>numero2 && numero1> numero3 && numero1>numero4){
         System.out.println("El número mayor es:"  + numero1);
 
-    } else if (numero2>numero1 && numero2>numero3)
+    } else if (numero2>numero1 && numero2>numero3 && numero2>numero4)
         {
-        System.out.println("  El número mayor es"  + numero2);
-        } else if (numero3>numero1 && numero3>numero2){
-        System.out.println("  El número mayor es"  + numero3);
-        } else if (numero4>numero1 && numero4>numero3) {
-        System.out.println("  El número mayor es"  + numero3);
+        System.out.println("  El número mayor es  "  + numero2);
+        } else if (numero3>numero1 && numero3>numero2 && numero3>numero4) {
+        System.out.println("  El número mayor es  "  + numero3);
+        } else if (numero4>numero1 && numero4>numero2 && numero4>numero3) {
+        System.out.println("  El número mayor es  "  + numero4);
         } else {
         System.out.println(" los numeros son iguales.");
         }
